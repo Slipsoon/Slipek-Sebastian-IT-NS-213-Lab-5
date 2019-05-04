eaf3e6de6fd1ef81dfd48c47ec95c732d05fb1a5
Sebastian Slipek <sebastian.slipek@microsoft.wsei.edu.pl>
2019-05-04 14:44:38 +0200

    add & finish music play function, fix gitignore

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const play = document.querySelector('button').addEventListener('click', () => console.log('duppa'));
+const sound = new Audio();
+const buttons = document.querySelectorAll('button.music');
+
+const playList = ['Music/boom.wav', 'Music/clap.wav', 'Music/hihat.wav', 'Music/kick.wav', 'Music/openhat.wav', 'Music/ride.wav', 'Music/snare.wav', 'Music/tink.wav', 'Music/tom.wav'];
+
+function playSound() {
+    sound.src = playList[this.dataset.music];
+    sound.play();
+}
+
+buttons.forEach(button => button.addEventListener('click', playSound));
