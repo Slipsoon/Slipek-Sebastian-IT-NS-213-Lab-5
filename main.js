68198d85278fd957fb13d820d3ba25581584df5b
Sebastian Slipek <sebastian.slipek@microsoft.wsei.edu.pl>
2019-05-04 19:25:33 +0200

    add music play by key press, add comments

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-const sound = new Audio();
-const buttons = document.querySelectorAll('button.music');
+    // Consts
+    const sound = new Audio();
+    const buttons = document.querySelectorAll('button.music');
 
-const playList = ['Music/boom.wav', 'Music/clap.wav', 'Music/hihat.wav', 'Music/kick.wav', 'Music/openhat.wav', 'Music/ride.wav', 'Music/snare.wav', 'Music/tink.wav', 'Music/tom.wav'];
+    // Music Array
+    const playList = ['', 'Music/boom.wav', 'Music/clap.wav', 'Music/hihat.wav', 'Music/kick.wav', 'Music/openhat.wav', 'Music/ride.wav', 'Music/snare.wav', 'Music/tink.wav', 'Music/tom.wav'];
 
-function playSound() {
-    sound.src = playList[this.dataset.music];
-    sound.play();
-}
+    // Play sound by image click f.
+    function playSoundByClick() {
+        sound.src = playList[this.dataset.music];
+        sound.play();
+    }
 
-buttons.forEach(button => button.addEventListener('click', playSound));
+    // Play sound by key press f. 
+    function playSoundByKey(e) {
+        let index = 1; // default key 1 (boom.wav)
+
+        // check if key is between 1-9
+        for (let i = 1; i < playList.length; i++) {
+            if (i == e.key)
+                index = i;
+        }
+
+        sound.src = playList[index];
+        sound.play();
+    }
+
+    // Music click event listener
+    buttons.forEach(button => {
+        button.addEventListener('click', playSoundByClick);
+    });
+    // Keyup event listener
+    document.addEventListener('keyup', playSoundByKey);
